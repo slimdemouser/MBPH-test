6215a8d343ac19b7bb42d1c6d1fe4efb9658041f
SLIM <slim@host145-82.wifi.ubc.ca>
2012-02-24 11:44:33 -0800

    initial commit

---
+++ imagesu.m
@@ -0,0 +1,21 @@
+function h = imagesu(image_matrix, image_name)
+% FUNCTION imagesu(image_matrix, image_name)
+% 
+% imagesu() is the same as imagesc except it opens a new window everytime you call it
+% arguments
+%	image_matrix	the matrix that gets passed to imagesc
+%	imageName		a string that will show up in the figure title and window title
+%
+%	Tim Lin 2012 timtylin@gmail.com
+	
+	if not(exist('image_name','var'))
+	    h = figure();
+	else
+	    h = figure('Name', image_name);
+	end
+    colormap(gray);
+    imagesc(real(image_matrix))
+	if not(exist('image_name','var'))
+		title(image_name)
+	end
+end
